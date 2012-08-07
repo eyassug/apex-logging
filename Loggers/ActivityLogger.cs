64e2edee4b065225fd52550505032d7548928c9d
Eyassu Getachew <eyassug@gmail.com>
2012-08-07 16:04:44 +0300

    - Added LogRepository, LoggerBase & ActivityLogger - Removed App.Config, since it's unnecessary to include a connection string in a project of type Class Library

---
+++ Loggers/ActivityLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HCMIS.Logging.Models;
+
+namespace HCMIS.Logging.Loggers
+{
+    public class ActivityLogger : LoggerBase, IActivityLog
+    {
+        public void SaveAction(int userId, int activityId, string page, string activityName)
+        {
+            var log = new ActivityLog
+                                  {
+                                      UserID = userId,
+                                      ActivityID = activityId,
+                                      Page = page,
+                                      ActivityName = activityName,
+                                      TimeStamp = DateTime.Now
+                                  };
+            Repository.Add(log);
+        }
+    }
+}
---
+++ Repository/LogRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HCMIS.Logging.Models;
+
+namespace HCMIS.Logging.Repository
+{
+    public class LogRepository:GenericRepository<LoggingContext,Log>
+    {
+
+    }
+}
