f82b42c4b2fd4b2220b23aa51eebe16922ee990b
Eyassu Getachew <eyassug@gmail.com>
2012-08-07 17:18:01 +0300

    - Updated LogRepository to return a specific type of Log (Activity,Error...)

--- Repository/LogRepository.cs
+++ Repository/LogRepository.cs
@@ -8,6 +8,24 @@ namespace HCMIS.Logging.Repository
 {
     public class LogRepository:GenericRepository<LoggingContext,Log>
     {
+        public IQueryable<ActivityLog> GetAllActivityLogs()
+        {
+            return Context.Logs.OfType<ActivityLog>();
+        }
 
+        public IQueryable<ErrorLog> GetAllErrorLogs()
+        {
+            return Context.Logs.OfType<ErrorLog>();
+        }
+
+        public IQueryable<SessionLog> GetAllSessionLogs()
+        {
+            return Context.Logs.OfType<SessionLog>();
+        }
+
+        public IQueryable<ProfileLog> GetAllProfileLogs()
+        {
+            return Context.Logs.OfType<ProfileLog>();
+        }
     }
 }
