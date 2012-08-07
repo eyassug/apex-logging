d848cdc184ba5cb21bc725573f8bdd870634a3e6
Eyassu Getachew <eyassug@gmail.com>
2012-08-07 10:44:21 +0300

    - Initial commit

---
+++ IActivityLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HCMIS.Logging
+{
+    public interface IActivityLog : ILog
+    {
+        void SaveAction(int userId, int activityId, string page, string activityName);
+    }
+}
---
+++ IErrorLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HCMIS.Logging
+{
+    public interface IErrorLog : ILog
+    {
+        void SaveError(int userId, int appVersion, int dbVersion, int errorLevel, string page, string activity, DateTime errorDate, Exception exception);
+    }
+}
