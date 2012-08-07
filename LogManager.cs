ae08952c69d2387003b4500e0d22d292fc0ddb88
Eyassu Getachew <eyassug@gmail.com>
2012-08-07 16:24:15 +0300

    - Added LoggerBase class - Added LogManager (static class)

---
+++ LogManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HCMIS.Logging.Loggers;
+
+namespace HCMIS.Logging
+{
+    public static class LogManager
+    {
+        public static IActivityLog GetActivityLogger()
+        {
+            IActivityLog logger = new ActivityLogger();
+            logger.Setup(new object());
+            return logger;
+        }
+
+        public static IErrorLog GetErrorLogger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ISessionLog GetSessionLogger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IProfiler GetProfilerLogger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
