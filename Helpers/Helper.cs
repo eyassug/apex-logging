72615f8708a8a04b392d81ee8133e7aa126bca8b
Kassa Demissie <k.demissie@gmail.com>
2012-08-10 15:15:08 +0300

    Added helper method "GetLineNumber" to find the statment that raised the exception

--- Helpers/Helper.cs
+++ Helpers/Helper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
+using System.Diagnostics;
 
 namespace HCMIS.Logging.Helpers
 {
@@ -18,6 +19,16 @@ namespace HCMIS.Logging.Helpers
             IPAddress[] addr = ipEntry.AddressList;
 
             return addr[addr.Length - 2].ToString();
+        }
+
+        public static int GetLineNumber(Exception ex)
+        {
+            var st = new StackTrace(ex, true);
+            // Get the top stack frame
+            var frame = st.GetFrame(0);
+            // Get the line number from the stack frame
+            var line = frame.GetFileLineNumber();
+            return line;
         }
     }
 }
